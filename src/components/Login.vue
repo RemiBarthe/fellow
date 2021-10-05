8dc4ce286f6d8a785cbcef1bd6ba18fc8d128844
RemiBarthe <barthe.rem@gmail.com>
2021-10-05 23:41:38 +0200

    Use redirect method

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="border-2 border-gray-700">hello {{ user.displayName }}</div>
+  <div class="border-2 border-gray-700">
+    hello {{ user.displayName }}
+    <p>{{ errorMessage }}</p>
+  </div>
   <button @click="googleSignIn">
     Sign In with Google
   </button>
 </template>
 
 <script>
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import {
+  getAuth,
+  getRedirectResult,
+  signInWithRedirect,
+  GoogleAuthProvider
+} from 'firebase/auth';
 import { provider } from '../firebase';
 
 export default {
@@ -18,26 +26,30 @@ export default {
     errorMessage: '',
     credential: ''
   }),
+  created() {
+    const auth = getAuth();
+    getRedirectResult(auth)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access Google APIs.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        this.token = credential.accessToken;
+
+        // The signed-in user info.
+        this.user = result.user;
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        this.errorCode = error.code;
+        this.errorMessage = error.message;
+        // The AuthCredential type that was used.
+        this.credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  },
   methods: {
     googleSignIn() {
       const auth = getAuth();
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          this.credential = GoogleAuthProvider.credentialFromResult(result);
-          this.token = this.credential.accessToken;
-          // The signed-in user info.
-          this.user = result.user;
-          // ...
-        })
-        .catch((error) => {
-          // Handle Errors here.
-          this.errorCode = error.code;
-          this.errorMessage = error.message;
-          // The AuthCredential type that was used.
-          this.credential = GoogleAuthProvider.credentialFromError(error);
-          // ...
-        });
+      signInWithRedirect(auth, provider);
     }
   }
 };
