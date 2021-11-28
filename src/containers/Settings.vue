3d3a5c7e1fb8524af123053dc81fd1a446e97d55
RemiBarthe <barthe.rem@gmail.com>
2021-11-28 03:07:45 +0100

    Add white bg

--- src/containers/Settings.vue
+++ src/containers/Settings.vue
@@ -3,53 +3,59 @@
     Paramètres
   </h2>
 
-  <h3 class="text-base font-bold">
-    Gestion des espaces
-  </h3>
+  <div class="bg-white p-5 rounded">
+    <h3 class="text-base font-bold">
+      Gestion des espaces
+    </h3>
 
-  <div class="flex flex-col gap-1 mb-5">
-    <div
-      v-for="space in spaces"
-      :key="space.id"
-      class="flex justify-between items-center hover:text-primary"
-    >
-      <p>{{ space.title }}</p>
+    <div class="flex flex-col gap-1 mb-5">
+      <div
+        v-for="space in spaces"
+        :key="space.id"
+        class="flex justify-between items-center hover:text-primary"
+      >
+        <p>{{ space.title }}</p>
 
-      <div>
-        <Icon
-          icon="akar-icons:edit"
-          class="mr-1 text-base-lg float-left cursor-pointer"
-        />
+        <div>
+          <Icon
+            icon="akar-icons:edit"
+            class="mr-1 text-base-lg float-left cursor-pointer"
+          />
 
-        <Icon
-          icon="fluent:delete-16-regular"
-          class="text-base-lg float-left cursor-pointer hover:text-thirdary"
-        />
+          <Icon
+            icon="fluent:delete-16-regular"
+            class="text-base-lg float-left cursor-pointer hover:text-thirdary"
+          />
+        </div>
       </div>
     </div>
+
+    <h3 class="text-base font-bold">
+      Gestion du compte
+    </h3>
+
+    <button
+      class="flex items-center text-thirdary hover:text-black"
+      @click="signOutUser"
+    >
+      <Icon
+        icon="fa-solid:sign-out-alt"
+        class="mr-2"
+      />
+      Se déconnecter
+    </button>
+
+    <button
+      class="flex items-center text-thirdary hover:text-black"
+      @click="signOutUser"
+    >
+      <Icon
+        icon="fluent:delete-16-regular"
+        class="mr-2 text-base-lg"
+      />
+      Supprimer mon compte
+    </button>
   </div>
-
-  <button
-    class="flex items-center text-thirdary hover:text-black"
-    @click="signOutUser"
-  >
-    <Icon
-      icon="fa-solid:sign-out-alt"
-      class="mr-2"
-    />
-    Se déconnecter
-  </button>
-
-  <button
-    class="flex items-center text-thirdary hover:text-black"
-    @click="signOutUser"
-  >
-    <Icon
-      icon="fluent:delete-16-regular"
-      class="mr-2 text-base-lg"
-    />
-    Supprimer mon compte
-  </button>
 </template>
 
 <script>
